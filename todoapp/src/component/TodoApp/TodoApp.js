3fae7602267bac727da9c627c8f827c2d562da0e
sreejithliterally <imsreejithofficial@gmail.com>
2022-09-04 22:12:54 +0530

    funtions

--- todoapp/src/component/TodoApp/TodoApp.js
+++ todoapp/src/component/TodoApp/TodoApp.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react'
 import "./TodoApp.css";
 
 export class TodoApp extends Component {
+
+    state={
+        input:"",
+        store:[]
+
+    }
+
+    handleChange=event=>{
+        this.setState({
+            input:event.target.value
+        })
+
+    storeItems=()=>{
+        
+    }
+
+        
+    }
     render() {
+        const{input}= this.state;
+
         return (
             <div className="todo-container">
 
                 <div className="input-box"><h1>Todo App </h1>
-                <input type= 'text'/>
-                <button>Add</button>
+                <input type= 'text'
+                value={input}
+                onChange={this.handleChange}
+                />
+
+                <div className="container">
+                     <div className="btn-holder">
+                <button type="button"
+                onClick={this.storeItems} >Click</button>
+                    </div>
+                </div>
                 </div>
 
                 <ul>
                     <li>Items</li>
+    
                 </ul>
             </div>
 
